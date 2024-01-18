0f6b83f6e1dcfbd62a026efad57e09a4c24038b5
shmuel <shmuelelmashali@gmal.com>
2024-01-18 14:35:07 +0200

    s

---
+++ backEnd/src/SERVICES/user.servies.js
@@ -0,0 +1,46 @@
+const emailController = require("../dal/controller/email.controller");
+const userController = require("../dal/controller/user.controller");
+
+// async function exisingMessage(data){
+//     const messageExist = await emailController.readOne({_id: data.id})
+//   if(!messageExist){
+//     throw new Error("Message not found")
+//   }
+//   return messageExist
+// }
+
+async function addNeeUser(data) {
+  const newUser = {
+    firstName: data.firstName,
+    lastname: data.lastname,
+    password: data.password
+  };
+  const addUser = await userController.create(data)
+  return addUser
+}
+async function getUser(filter) {
+  console.log(filter)
+  const user = await userController.readOne({email:filter})
+  return user
+}
+
+
+
+
+
+async function tagToUpdate(user, messageId) {
+  // exisingMessage(user)
+
+  // const update = await userController.userTagToUpdate(
+  //   user,
+  //   "trashRead",
+  //   messageId
+  // );
+//   return update;
+}
+
+module.exports = {
+  addNeeUser,
+  tagToUpdate,
+  getUser
+};
